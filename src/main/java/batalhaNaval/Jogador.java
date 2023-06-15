500835bd3202a8cc4988c5d2140dece96c0fbeb3
ArthurFSchmidt <arthur-fschmidt@educar.rs.gov.br>
2023-06-15 16:28:11 -0300

    Sistema de ataque em desenvolvimento.

--- src/main/java/batalhaNaval/Jogador.java
+++ src/main/java/batalhaNaval/Jogador.java
@@ -6,23 +6,33 @@ import java.util.Scanner;
 public class Jogador {
     Random rand = new Random();
     Scanner scan = new Scanner(System.in);
+    Mapa jogador = new Mapa();
+    public String nome;
 
-    static Mapa jogador = new Mapa();
-
-    static {
+    public Jogador() {
+        // Configura o mapa
         jogador.preencherMapa();
     }
 
-    public String nome;
+
     public void preencher(int tamanho, boolean isManual){
+
+        // Método para receber os valores e colocar um barco no mapa
+
         int valorX = -1, valorY = -1;
         boolean isVertical = false;
+
+        // Se estiver no modo manual, pedirá ao usuário as informações necessárias.
+        // Repete a coleta de valores até serem inseridos valores válidos.
+        // No modo automático randomizará os valores.
+
         if(isManual){
             do{
+                System.out.println("=== Valores do barco de tamanho "+tamanho+" ===");
                 System.out.println("Digite a direção do barco: [1] Vertical [0] Horizontal");
                 isVertical = 1 == scan.nextInt();
                 System.out.println("Insira a posição da linha inicial do barco:");
-                valorX = scan.nextInt();
+                valorX = charToIntCoord(scan.next().toUpperCase().charAt(0));
                 System.out.println("Insira a posição da coluna inicial do barco:");
                 valorY = scan.nextInt();
             }while(jogador.colocarBarco(tamanho,valorX,valorY,isVertical));
@@ -32,8 +42,52 @@ public class Jogador {
                 valorX = rand.nextInt(10);
                 valorY = rand.nextInt(10);
             }while(jogador.colocarBarco(tamanho,valorX,valorY,isVertical));
-
         }
+    }
+    public int[] bombear(boolean isPvp) {
 
+        // Recebe do jogador ou randomiza as coordenas do ataque, retorna como array.
+
+        int valorX;
+        int valorY;
+        int[] coords = new int[2];
+        if (isPvp){
+            do {
+                System.out.println("Insira a posição da linha a ser bombeada:");
+                valorX = charToIntCoord(scan.next().toUpperCase().charAt(0));
+            } while (valorX < 1 || valorX > 10);
+            coords[0] = valorX;
+            do {
+                System.out.println("Insira a posição da coluna a ser bombeada:");
+                valorY = scan.nextInt();
+            } while (valorY < 1 || valorY > 10);
+            coords[1] = valorY;
+        }else{
+            coords[0] = rand.nextInt(10);
+            coords[1] = rand.nextInt(10);
+        }
+        return coords;
+    }
+    public int charToIntCoord(char coordChar){
+
+        // Recebe uma coordenada tipo char e retorna uma coodenada tipo int equivalente.
+
+        return coordChar-64;
+    }
+    public void atacar(Jogador atacado, boolean isPvp){
+
+        // Método que recebe as coordenadas bombeadas e verifica se já foram atacadas.
+        // Passa as coordenadas para o adversário e recebe a char atacada.
+
+        int[] coords;
+
+        do{
+            coords = this.bombear(isPvp);
+        }while(jogador.tabelaVisao[coords[0]][coords[1]] == ' ');
+
+        this.jogador.receberVisao(coords, atacado.jogador.receberAtaque(coords));
+    }
+    public boolean verificarDerrota(){
+        return jogador.barcosRestantes <= 1;
     }
 }
--- src/main/java/batalhaNaval/Mapa.java
+++ src/main/java/batalhaNaval/Mapa.java
@@ -2,10 +2,17 @@ package batalhaNaval;
 
 public class Mapa {
     public char[][] tabelaAtaques = new char[10][10];
+    // Matriz que marca os espaços já atacados.
     public char[][] tabelaVisao = new char[10][10];
+    // Matriz que mostrará a visão do jogador dessa classe ao campo do jogador inimigo.
     public char[][] tabelaBarcos = new char[10][10];
+    // Matriz que guardará as posições dos barcos do jogador dessa classe.
+    public int barcosRestantes = 10;
 
     public void preencherMapa(){
+
+        // Método para definir todas as matrizes de mapa como água ('A') ou valor equivalente.
+
         for (int i = 0 ; i < 10; i++){
             for (int j = 0 ; j < 10; j++){
                 tabelaBarcos[i][j] = 'A';
@@ -16,11 +23,16 @@ public class Mapa {
     }
 
     public boolean colocarBarco(int tamanho, int x, int y, boolean isVertical){
+
+        // Método para que valida e coloca um barco no mapa.
+
+        // Validação do tamanho do barco.
         if (tamanho > 4 || tamanho < 1)
             return false;
 
         boolean isValid = true;
 
+        // Chama o método isPlaceble para validar os espaços do barco.
         for(int i=0 ; i<tamanho ; i++){
             if (isVertical){
                 if(!isPlaceble(x+i,y))
@@ -31,6 +43,7 @@ public class Mapa {
             }
         }
 
+        // Se for válido, coloca o barco.
         if(isValid){
             for(int i=0 ; i<tamanho ; i++){
                 if (isVertical) {
@@ -45,11 +58,31 @@ public class Mapa {
     }
 
     public boolean isPlaceble(int x, int y){
+
+        // Método que verifica se a posição do barco é válida.
+
         if(x>=0 && x<=10 && y>=0 && y<=10){
             return tabelaBarcos[x][y] == 'A';
         }
         return false;
     }
 
+    public char receberAtaque(int[] coords){
+
+        // Recebe as coordenadas bombeadas e retorna o char correspondente.
+        // Caso acerte um barco, diminui a quantidade de barcos.
+
+        char atacado = tabelaBarcos[coords[0]][coords[1]];
+        if(atacado == 'B')
+            barcosRestantes--;
+        return atacado;
+    }
+
+    public void receberVisao(int[] coords, char visao){
+
+        // Recebe o char da célula que atacou e coloca na matriz.
+
+        tabelaVisao[coords[0]][coords[1]] = visao;
+    }
 
 }
